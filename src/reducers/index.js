ac6dd9f8135e836404224bbf0730319690fbb485
rixong <rixong@gmail.com>
2020-09-09 18:40:40 -0500

    removed ()

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,7 @@ export default function shoppingListReducer(
   // console.log('From reducer', action)
   switch (action.type) {
     case 'ADD_TO_LIST':
-      return ({ ...state, curList: state.curList.concat(action.payload) })
+      return { ...state, curList: state.curList.concat(action.payload) }
     default:
       return state
   }
