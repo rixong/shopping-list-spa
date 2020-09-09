fd115aa0069f8a0fcf598852b24a3f987d85427d
rixong <rixong@gmail.com>
2020-09-08 20:31:46 -0500

    implemented itme search function

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { store } from '../seed';
 
 const AddItems = () => {
 
   const [queryTerm, setQueryTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([]);
 
   const onHandleChange = (e) => {
+    console.log(e.target.value)
     setQueryTerm(e.target.value)
+    if (e.target.value !== '') {
+      setSearchResults(store.filter(item => item.includes(e.target.value)));
+    } else {
+      setSearchResults('');
+    }
+  }
+
+  const onClickItem = (e) => {
+    console.log(e.target.textContent)
+    setQueryTerm(e.target.textContent)
+    setSearchResults('');
   }
 
   return (
@@ -15,13 +29,25 @@ const AddItems = () => {
         <div className="input-group">
           <input
             className="form-control"
-            type="text" value="queryTerm"
+            type="text"
             placeholder="Search for item..."
             value={queryTerm}
-            onChange={onHandleChange}
-            aria-label="enter item">
+            onChange={(e) => onHandleChange(e)}
+          // aria-label="enter item"
+          >
           </input>
         </div>
+        <ul className="list-group-flush">
+          {searchResults ?
+            searchResults.map((item, idx) =>
+              <li
+                key={idx}
+                className="list-group-item role"
+                role="button"
+                onClick={onClickItem}
+              >{item}</li>)
+            : null}
+        </ul>
       </form>
     </React-fragment>
   )
--- src/seed.js
+++ src/seed.js
@@ -1,4 +1,4 @@
-store = [
+export const store = [
   'apples',
   'pears',
   'berries',
