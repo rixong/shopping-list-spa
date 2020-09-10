229c582876de9edd10c5f6aa8c9098f29ddc0d3f
rixong <rixong@gmail.com>
2020-09-09 20:42:42 -0500

    finished feature

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,11 +6,10 @@ export default function shoppingListReducer(
   state = { curList: list },
   action
 ) {
-  console.log('From reducer', action)
+  // console.log('From reducer', action)
   switch (action.type) {
     case 'ADD_TO_LIST':
       let newList = Object.assign({}, state.curList)
-      console.log(newList);
       if (newList[action.payload.category]){
         newList[action.payload.category].push(action.payload)
       } else {
