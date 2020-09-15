6256ef9cc64449b16dd6b9b439fdc01bf2b8ab1b
rixong <rixong@gmail.com>
2020-09-15 09:16:49 -0500

    made category selector dynamic

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -9,7 +9,8 @@ const AddItems = ({
   clearNotification,
   masterList,
   curList,
-  curUser
+  curUser,
+  categories
 }) => {
 
   const queryDefault = { name: '', quantity: '', category: 0 }
@@ -56,87 +57,83 @@ const AddItems = ({
       quantity: queryTerm.category
     }, curUser.id, curList.id);
 
-  setQueryTerm(queryDefault);
-}
+    setQueryTerm(queryDefault);
+  }
 
-return (
-  <React-fragment>
-    <div className="display-4 text-center text-warning mb-4">Add items</div>
-    <form>
-      <div className="input-group">
-        <input
-          className="form-control"
-          id="name-input"
-          type="text"
-          placeholder="Search for item..."
-          value={queryTerm.name}
-          onChange={(e) => onHandleChange(e)}
-          name="name"
-          aria-label="enter item name"
-          required
-        ></input>
+  return (
+    <React-fragment>
+      <div className="display-4 text-center text-warning mb-4">Add items</div>
+      <form>
+        <div className="input-group">
+          <input
+            className="form-control"
+            id="name-input"
+            type="text"
+            placeholder="Search for item..."
+            value={queryTerm.name}
+            onChange={(e) => onHandleChange(e)}
+            name="name"
+            aria-label="enter item name"
+            required
+          ></input>
 
-        <input
-          className="form-control"
-          id="quantity-input"
-          type="text"
-          placeholder="Quantity..."
-          value={queryTerm.quantity}
-          onChange={(e) => onHandleChange(e)}
-          aria-label="enter quantity"
-          name="quantity"
-        ></input>
+          <input
+            className="form-control"
+            id="quantity-input"
+            type="text"
+            placeholder="Quantity..."
+            value={queryTerm.quantity}
+            onChange={(e) => onHandleChange(e)}
+            aria-label="enter quantity"
+            name="quantity"
+          ></input>
 
-        <select
-          className="form-control"
-          onChange={(e) => onHandleChange(e)}
-          name="category"
-          id="category-select"
-          value={queryTerm.category}
-        >
-          <option value="0">Category...</option>
-          <option value="1">Produce</option>
-          <option value="2">Meat</option>
-          <option value="3">Dairy</option>
-          <option value="4">Bakery</option>
-          <option value="5">Housewares</option>
-        </select>
+          <select
+            className="form-control"
+            onChange={(e) => onHandleChange(e)}
+            name="category"
+            id="category-select"
+            value={queryTerm.category}
+          >
+            <option value="0">Category...</option>
+            {categories.map(cat => <option value={cat.id} key={cat.id}>{cat.name}</option>)}
+          </select>
 
 
-        <div className="input-group-append">
-          <button
-            className="btn btn-outline-primary"
-            type="button"
-            id="button-addon2"
-            onClick={onClickSubmit}
-          >Add item</button>
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-primary"
+              type="button"
+              id="button-addon2"
+              onClick={onClickSubmit}
+            >Add item</button>
+          </div>
+
         </div>
-
-      </div>
-      <ul className="list-group-flush itemSearchList">
-        {searchResults ?
-          searchResults.map((item) =>
-            <li
-              key={item.id}
-              className="list-group-item role"
-              role="button"
-              onClick={(e) => onSelectItem(e)}
-              data-id={item.id}
-            >{item.name}</li>)
-          : null}
-      </ul>
-    </form>
-  </React-fragment>
-)
+        <ul className="list-group-flush itemSearchList">
+          {searchResults ?
+            searchResults.map((item) =>
+              <li
+                key={item.id}
+                className="list-group-item role"
+                role="button"
+                onClick={(e) => onSelectItem(e)}
+                data-id={item.id}
+              >{item.name}</li>)
+            : null}
+        </ul>
+      </form>
+    </React-fragment>
+  )
 }
 
 const mapStateToProps = state => {
   return {
     curUser: state.curUser,
     curList: state.curList,
-    masterList: state.masterList
+    masterList: state.masterList,
+    categories: state.categories
   }
 }
-
 
 export default connect(mapStateToProps, { addNotification, clearNotification, addItemToMasterList })(AddItems);
