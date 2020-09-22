ef3311ebf34118796c133a59097a6a3ec9ccc243
rixong <rixong@gmail.com>
2020-09-22 17:35:14 -0500

    new user comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Login = () => {
+
+  const [formType, setFormType] = useState('login');
+  const message = {
+    login: "Don't have an account?",
+    newUser: "Already have an account?"
+  }
+
   return (
-    <div className="login-box bg-light mt-4 shadow-lg">
-      <div className="row mt-5 justify-content-center">
+    <div className="login-box bg-light mt-4 shadow-lg rounded">
+      <div className="row mt-4 justify-content-center">
         <img src="shopping-cart-logo.png" className="mt-5" style={{ width: "100px" }}></img>
       </div>
       <div className="display-3 text-center text-warning pt-5">Cart Compass</div>
@@ -12,14 +19,20 @@ const Login = () => {
         <div className="form-group login-form">
           <input type="email" className="form-control form-control-lg my-3" aria-describedby="emailHelp" placeholder="Email"></input>
           <input type="password" className="form-control form-control-lg mb-3" aria-describedby="passwordHelp" placeholder="Password"></input>
+          { formType === 'newUser' ?
+            <input type="password" className="form-control form-control-lg mb-3" aria-describedby="passwordHelp" placeholder="Confirm password"></input>
+            : null
+          }
           <button type="submit" class="btn btn-primary btn-lg w-100">Continue</button>
         </div>
       </form>
       <div className="mt-5 d-block">
-        <p className="text-center">Don't have an account?</p>
-        {/* <div className="text-center" > */}
-          <div className="text-center text-primary h5" role="button">Create account</div>
-        {/* </div> */}
+        <p className="text-center">{message[formType]}</p>
+        {formType === 'login' ? 
+        <div className="text-center text-primary h5" onClick={() => setFormType('newUser')} role="button">Create account</div>
+        :
+        <div className="text-center text-primary h5" onClick={() => setFormType('login')} role="button">Login</div>
+      }
       </div>
 
 
